d57e455630f6a8718c9326ac70ec1390ab7465cf
Cristofer Jaimez <jaimez07788@gmail.com>
2022-10-23 16:46:48 -0500

    back jwt

--- middleware/authjwt.js
+++ middleware/authjwt.js
@@ -2,9 +2,9 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 //generate token
 module.exports =   function (data) {
-    let token =  JSON.stringify(data)
+    let token = JSON.stringify(data)
 
-    const  token_ =  jwt.sign(token,process.env.JWT)
+    const  token_ =  jwt.sign({token},process.env.JWT, {expiresIn: '15m'})
     return token_;
 }
 
